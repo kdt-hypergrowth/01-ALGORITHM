b73fbf227ad1f95ec8a27c860db000596f2f18ef
hjkim1350 <hjkim1350@daum.net>
2022-07-25 14:42:44 +0900

    2022.07.25 오후 실습 결과 제출

--- 1회차/김현정/20220725/0_숫자의개수.py
+++ 1회차/김현정/20220725/0_숫자의개수.py
@@ -2,3 +2,14 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+intA = int(input())
+intB = int(input())
+intC = int(input())
+
+mul_total = intA*intB*intC
+mul_total = str(mul_total)
+
+for i in range(0, 10):
+    i = str(i)
+    print(mul_total.count(i))
--- 1회차/김현정/20220725/3_OX퀴즈.py
+++ 1회차/김현정/20220725/3_OX퀴즈.py
@@ -2,3 +2,18 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+test_case = int(input())
+
+for i in range(1, test_case+1):
+    result = 0
+    score = 0
+    OX = input()
+
+    for j in range(0, len(OX)):
+        if OX[j] == 'O':
+            score += 1
+            result += score
+        else:
+            score = 0
+    print(result)
