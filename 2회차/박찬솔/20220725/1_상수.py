503b807dd61bd45cdfdd5ec17b72ab5249cfe434
GitHub cksthf3211 <cksthf321@gmail.com>
2022-07-25 18:03:20 +0900

    알고리즘

--- 2회차/박찬솔/20220725/1_상수.py
+++ 2회차/박찬솔/20220725/1_상수.py
@@ -2,3 +2,13 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+T1, T2 = input().split()
+T1 = int(T1[::-1]) # 역순
+T2 = int(T2[::-1])
+
+# 역순으로 만약 T1이 더 크다면 출력
+if T1 > T2: 
+    print(T1)
+# 그렇지 않으면 T2를 출력    
+else:
+    print(T2)
--- 2회차/박찬솔/20220725/3_OX퀴즈.py
+++ 2회차/박찬솔/20220725/3_OX퀴즈.py
@@ -2,3 +2,17 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+T = int(input())
+
+for i in range(T):
+    a = list(input())
+    cnt = 0
+    sum = 0
+    for i in a:
+        if i == 'O':
+            cnt += 1
+            sum = sum+cnt
+        else:
+            cnt = 0
+    print(sum)
