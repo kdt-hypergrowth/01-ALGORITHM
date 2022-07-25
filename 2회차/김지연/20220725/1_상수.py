9b8302d70154735a7e6c43ca53db34cc2325ecf5
Jni97 <kjo5395@naver.com>
2022-07-25 14:23:34 +0900

    2회차 김지연

--- 2회차/김지연/20220725/1_상수.py
+++ 2회차/김지연/20220725/1_상수.py
@@ -1,4 +1,16 @@
 # https://www.acmicpc.net/problem/2908
-import sys
+# import sys
 
-sys.stdin = open("1_상수.txt")
+# sys.stdin = open("1_상수.txt")
+
+a, b = input().split()
+
+a = a[::-1]
+b = b[::-1]
+a = int(a)
+b = int(b)
+
+if (a > b):
+    print(a)
+else:
+    print(b)
