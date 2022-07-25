b453372947404e05274e24f90540e7c53fe47361
GJ <gwangjin392@gmail.com>
2022-07-25 20:54:11 +0900

    0725_GWANGJIN_LEE

--- 3회차/이동근/20220725/1_상수.py
+++ 3회차/이동근/20220725/1_상수.py
@@ -1,4 +1,16 @@
 # https://www.acmicpc.net/problem/2908
-import sys
+# import sys
 
-sys.stdin = open("1_상수.txt")
+# sys.stdin = open("1_상수.txt")
+
+N = int(input())
+
+for tc in range(1, N + 1):
+    a, b = map(int, input().split())
+    ra = int(str(a)[::-1])
+    rb = int(str(b)[::-1])
+        
+    if ra > rb:
+        print(ra)
+    else:
+        print(rb)
