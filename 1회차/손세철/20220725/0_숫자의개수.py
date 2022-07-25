8360b2e11fd476c9ae70007eb1b8f1192818872e
chul1631 <sonky201@gmail.com>
2022-07-25 18:02:54 +0900

    알고리즘_손세철

--- 1회차/손세철/20220725/0_숫자의개수.py
+++ 1회차/손세철/20220725/0_숫자의개수.py
@@ -2,3 +2,16 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+# A, B, C 각각 입력 받음
+multiple= (A*B*C)
+result = list(str(multiple))
+#result 변수 설정 A,B,C 곱한 값을 str으로 변환해서 list로 묶어줌
+for i in range (10):
+# for문 사용 반복
+    print(result.count(str(i)))
+# count 사용 0~9까지 수 str으로 변환 후 str(i) 카운트 해주면 0~9 각각의 개수 출력
--- 1회차/손세철/20220725/1_상수.py
+++ 1회차/손세철/20220725/1_상수.py
@@ -2,3 +2,16 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+A, B = input().split()
+# 입력값을 공백으로 나눔
+
+A = int(A[::-1]) 
+B = int(B[::-1])
+# 정수로 바꾸고 역순으로
+
+if A > B:
+    print(A)
+else : 
+    print(B)
+# A가 더 크면 A출력, 아닐 시 B를 출력
