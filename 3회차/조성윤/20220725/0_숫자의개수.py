5b4ea5b28662966ce7fef712f1bd85e3c7774266
jsy78 <whtjd1541@gmail.com>
2022-07-25 16:16:50 +0900

    add 0725 algorithm

--- 3회차/조성윤/20220725/0_숫자의개수.py
+++ 3회차/조성윤/20220725/0_숫자의개수.py
@@ -2,3 +2,16 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+string = str(A*B*C)
+num_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for char in string :
+    num_list[int(char)] += 1
+
+for num in num_list :
+    print(num)
--- 3회차/조성윤/20220725/1_상수.py
+++ 3회차/조성윤/20220725/1_상수.py
@@ -2,3 +2,13 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+A, B = map(int, input().split())
+
+str_A = str(A)
+str_B = str(B)
+
+reverse_A = int(str_A[::-1])
+reverse_B = int(str_B[::-1])
+
+print(reverse_A) if reverse_A > reverse_B else print(reverse_B)
