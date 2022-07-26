f18ada382546500d7ef1698de53c60467594ee17
kimheekimhee <khd128k@gmail.com>
2022-07-26 21:39:20 +0900

    Algorithm

--- 2회차/김희동/20220725/0_숫자의개수.py
+++ 2회차/김희동/20220725/0_숫자의개수.py
@@ -2,4 +2,11 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
-# PR
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+result = list(str(a * b * c))
+for i in range(0, 10):
+    print(result.count(str(i)))
--- 2회차/김희동/20220725/1_상수.py
+++ 2회차/김희동/20220725/1_상수.py
@@ -2,3 +2,16 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+a, b = map(int, input().split())
+
+print(a, b, type(a), type(b))
+
+number_1 = str(a)[::-1]
+print(number_1)
+number_2 = str(b)[::-1]
+print(number_2)
+if number_1 > number_2:
+    print(number_1)
+if number_1 < number_2:
+    print(number_2)
