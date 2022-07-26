ed962a10eb2ecd2dd0b1b0ced43b381000302bec
kimheekimhee <khd128k@gmail.com>
2022-07-26 21:45:49 +0900

    Algorithm

--- 2회차/김희동/20220725/1_상수.py
+++ 2회차/김희동/20220725/1_상수.py
@@ -8,10 +8,10 @@ a, b = map(int, input().split())
 print(a, b, type(a), type(b))
 
 number_1 = str(a)[::-1]
-print(number_1)
+print(number_1, type(number_1))
 number_2 = str(b)[::-1]
-print(number_2)
-if number_1 > number_2:
+print(number_2, type(number_2))
+if int(number_1) > int(number_2):
     print(number_1)
-if number_1 < number_2:
+if int(number_1) < int(number_2):
     print(number_2)
