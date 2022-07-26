fc650273f3284ac9dce3500e608bcaac790d8c9f
rrwe23 <jhrrt0626@gmail.com>
2022-07-26 16:39:49 +0900

    0726백준 문제풀이

--- 3회차/이주현/20220726/4_나는요리사다.py
+++ 3회차/이주현/20220726/4_나는요리사다.py
@@ -1,4 +1,15 @@
 # https://www.acmicpc.net/problem/2953
-import sys
 
-sys.stdin = open("4_나는요리사다.txt")
+cnt = 0
+result = 0
+
+for i in range(5):    # 5명의 참가자
+    sum_ = 0
+    a = map(int,input().split())  # 4번의 점수를 받음
+    a = list(a)         #받은 점수를 리스트로 받음
+    for j in range(4): 
+        sum_ += a[j]    # 받은 4개의 점수를 더함
+    if result < sum_:   
+        result = sum_   #기존값보다 sum_값이 크면    result 값을 대신함
+        cnt = i+1       # 값이 클 때 cnt의 값이 1 올라감, 즉 3번째 줄이 제일 크다면 값이 3번 더해져서 가장 큰 줄의 행이 나온다. 
+print(cnt,result)
--- 3회차/이주현/20220726/7_하얀칸.py
+++ 3회차/이주현/20220726/7_하얀칸.py
@@ -1,4 +1,24 @@
 # https://www.acmicpc.net/problem/1100
-import sys
+# import sys
 
-sys.stdin = open("7_하얀칸.txt")
+# sys.stdin = open("7_하얀칸.txt")
+
+T = int(input())
+numbers = list(map(int,input().split()))
+temp = 0
+a = []
+
+for i in range(1,T):
+    if numbers[i] > numbers[i-1]:
+        temp += numbers[i] - numbers[i-1]
+        if i == T -1:
+            a.append(temp)
+    else:
+        a.append(temp)
+        temp = 0
+
+print(max(a))
+
+
+
+
