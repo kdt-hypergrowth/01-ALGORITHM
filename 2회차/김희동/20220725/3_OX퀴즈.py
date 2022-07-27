9434bc44757c7c4214550fa2f661f2df10c38048
kimheekimhee <khd128k@gmail.com>
2022-07-27 14:07:47 +0900

    algorithm

--- 2회차/김희동/20220725/3_OX퀴즈.py
+++ 2회차/김희동/20220725/3_OX퀴즈.py
@@ -2,3 +2,20 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+T = int(input())
+
+for t in range(T):
+    
+    ox = input()
+
+    count_o = 1
+    sum_ = 0
+
+    for answer in ox:
+        if answer == 'O':
+            sum_ = count_o + sum_
+            count_o += 1
+        if answer == 'X':
+            count_o = 0
+    print(sum_)
