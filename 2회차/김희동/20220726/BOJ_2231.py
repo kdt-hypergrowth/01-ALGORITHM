81d0fe61f81e7639b50c6b9cbb40b99fb14c1f41
kimheekimhee <khd128k@gmail.com>
2022-07-27 14:08:13 +0900

    algorithm

--- 2회차/김희동/20220726/BOJ_2231.py
+++ 2회차/김희동/20220726/BOJ_2231.py
@@ -4,10 +4,13 @@ import sys
 sys.stdin = open("BOJ_2231_input.txt")
 
 N = int(input())
-print(N, type(N))
-M = list(str(N))
-print(M, type(M))
-sum_numbers = 0
-for number in M:
-    int(number)
-    print(number)
+result = 0
+
+for i in range(1, N + 1):
+    a = list(map(int, str(i)))
+    s = i + sum(a)
+    if(s == N):
+        result = i
+        break
+
+print(result)
--- 2회차/김희동/20220726/BOJ_2846.py
+++ 2회차/김희동/20220726/BOJ_2846.py
@@ -1,4 +1,5 @@
 # https://www.acmicpc.net/problem/2846
 import sys
 
-sys.stdin = open("6_오르막길.txt")
+sys.stdin = open("BOJ_2846_input.txt")
+
