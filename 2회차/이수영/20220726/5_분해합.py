e850bd6e0e88190a9fbc2de60a8250492cab42e7
suyoung049 <suyoung049@gmail.com>
2022-07-27 21:25:55 +0900

    7.27 백준 및 SWEA 문제 풀이 추가

--- 2회차/이수영/20220726/5_분해합.py
+++ 2회차/이수영/20220726/5_분해합.py
@@ -11,6 +11,8 @@ for i in range(1, N+1): #  1부터 216까지 정수 범위의 레인지 생성
     decom = i + sum(seq)           # sum 함수 사용을 위해 다시 정수로 map활용
     if decom == N:
         print(i)
-        break                      # 값이 같을때 멈추면 최소값
-else:
+        break
+else:                     # 값이 같을때 멈추면 최소값
+                          # if-else 가 아니라 for - else로 사용
+    
     print('0')                  # 없을 시 0
