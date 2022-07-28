fd5251edd0c9c3be3cdab85131ba8d995cd51ea3
Jni97 <kjo5395@naver.com>
2022-07-28 17:08:30 +0900

    2022-07-28

--- 2회차/김지연/20220728/1_나머지.py
+++ 2회차/김지연/20220728/1_나머지.py
@@ -2,10 +2,9 @@ list_ = []
 
 for i in range(10):
     n = int(input())
-    n %= 42
-    n = int(n)
-
+    n %= 42 # 입력받은 숫자를 42로 나눈 나머지
+    
     if n not in list_:
-        list_.append(n)
+        list_.append(n) # 42로 나눈 나머지가 없으면 list에 추가
 
-print(len(list_))
+print(len(list_)) # list의 총 개수 출력
