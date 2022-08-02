21c286d8ad0a9c2b8ddbc091db0d224f995a078a
Sunjoo IM <tjss1580@gmail.com>
2022-08-02 17:14:18 +0900

    20220802

--- 3회차/임선주/20220802/2750_수정렬하기.py
+++ 3회차/임선주/20220802/2750_수정렬하기.py
@@ -4,4 +4,21 @@ import sys
 sys.stdin = open('20220802/2750_수정렬하기.txt')
 
 N = int(input())
+numbers = []
+
+for n in range(N):
+    numbers.append(int(input()))
+    numbers.sort()
+for _ in numbers:
+    print(_)
+
+
+# 오름차순 정렬 min heap
+
+import heapq
+
+# 1) 모든 원소를 차례대로 heap에 삽입한다.
+# 2) 힙에 삽입된 모든 원소를 차례대로 result에 담는다.
+
+N = int(input())
 
