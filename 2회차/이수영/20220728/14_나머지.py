993a700e94484f30f6b5745d91b1b51094a8c503
suyoung049 <suyoung049@gmail.com>
2022-08-03 21:43:10 +0900

    0802 필수문제 풀이

--- 2회차/이수영/20220728/14_나머지.py
+++ 2회차/이수영/20220728/14_나머지.py
@@ -7,6 +7,7 @@ senum = set()
 
 for _ in range(10):
     num_list.append(int(input())) # [0,1,2]
+    
 
 for i in range(len(num_list)):
     senum.add(num_list[i]% 42)  
--- 2회차/이수영/20220802/24_수정렬하기_2.py
+++ 2회차/이수영/20220802/24_수정렬하기_2.py
@@ -12,5 +12,6 @@ for _ in range(N):
     list_.append(int(input()))
 for i in range(len(list_)):
     heapq.heappush(hea, list_[i])
+print(hea)
 for j in range(len(hea)):
     print(heapq.heappop(hea))
