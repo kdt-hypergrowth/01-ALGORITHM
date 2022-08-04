c6b7be10cafc9a610a28a1c8136e065c9c8b1e36
cksthf3211 <cksthf321@gmail.com>
2022-08-04 17:54:02 +0900

    알고리즘

---
+++ 2회차/박찬솔/20220804/직사각형.py
@@ -0,0 +1,15 @@
+paper = [[0 for _ in range(10)] for _ in range(101)]
+
+for _ in range(4):
+    x1, y1, x2, y2 = map(int, input().split()) #사각형 부분만 1로 바꾸어줌
+                                               # 2차원으로 배열을 돌림
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            paper[i][j] = 1
+
+answer = 0
+
+for row in paper:
+    answer += sum(row)
+
+print(answer)
