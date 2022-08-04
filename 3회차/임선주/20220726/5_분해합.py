7cf836f515e66e283c351f4d35562d9bbe5caf47
Sunjoo IM <tjss1580@gmail.com>
2022-08-04 17:13:09 +0900

    20220726

--- 3회차/임선주/20220726/5_분해합.py
+++ 3회차/임선주/20220726/5_분해합.py
@@ -1,8 +1,15 @@
 # https://www.acmicpc.net/problem/2231
-import sys
 
+import sys
 sys.stdin = open("3회차/임선주/20220726/5_분해합.txt")
 
-num = int(input())
-# a x 100 + b x 10 + c x 1
+N = int(input())
+# 생성자 : (100 x a + 10 x b + c) + a + b + c
+a = int
+b = int
+c = int
+number = int(100 * a + 10 * b + c)
+number_N = int(number + a + b + c)
+
+print(min(number_N))
 
--- 3회차/임선주/20220726/6_오르막길.py
+++ 3회차/임선주/20220726/6_오르막길.py
@@ -1,5 +1,25 @@
 # https://www.acmicpc.net/problem/2846
-import sys
 
-sys.stdin = open("3회차/임선주/20220726/6_오르막길.txt")
+import sys
+sys.stdin = open('20220726/6_오르막길.txt')
+
+N = int(input())
+hill = list(map(int, input().split()))
+top = hill[0]
+bottom = hill[0]
+height = 0
+
+for n in range(N):
+    if hill[n] > hill[n-1]:
+        top = hill[n]
+    elif hill[n] < hill[n-1]:
+        bottom = hill[n]
+    height = top - bottom
+print(max(height))
+        
+
+
+
+    
+    
 
