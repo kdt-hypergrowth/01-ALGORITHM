63164638b7317c051720cb7fe0d2593fdcf94ab3
unknown <khd128k@gmail.com>
2022-08-07 22:30:54 +0900

    algorithm

---
+++ 2회차/김희동/20220804/N번째 큰 수.py
@@ -0,0 +1,12 @@
+from pprint import pprint
+import sys
+
+sys.stdin = open("N번째 큰 수_input.txt")
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    list_ = list(map(int, input().split()))
+    list__ = sorted(list_)
+        
+    print(list__[7])
